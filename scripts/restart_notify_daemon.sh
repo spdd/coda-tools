c137f7aeb19888a1533cb2e968503f2ae1fb3382
spdd <defaultqp@gmail.com>
2020-01-25 14:56:24 +0200

    Update restart_notify_daemon.sh
    added daemon command

--- scripts/restart_notify_daemon.sh
+++ scripts/restart_notify_daemon.sh
@@ -5,19 +5,25 @@ while [ "true" ]
         chat_id="<YOUR_CHAT_ID>"
         tg_api="https://api.telegram.org/bot${token}"
 
-        CODA_PK="<YOUR_PUBLIC_KEY>"
+        CODA_PK="YOUR_PUBLIC_KEY"
         sync_status="Synced|Catchup|Bootstrap"
         status=`coda client status | grep -E $sync_status`
+        password="<YOUR WALLET PASSWORD>"
         if [ -z "$status" ]; then
             echo "Coda crashed, trying to run coda..."
-            echo -e "<YOUR PASSWORD>\n" | coda daemon -discovery-port 8303
+            echo -e "${password}\n" | coda daemon -discovery-port 8303 \
+                            -peer /dns4/seed-one.genesis.o1test.net/tcp/10002/ipfs/12D3KooWP7fTKbyiUcYJGajQDpCFo2rDexgTHFJTxCH8jvcL1eAH \
+                            -peer /dns4/seed-two.genesis.o1test.net/tcp/10002/ipfs/12D3KooWL9ywbiXNfMBqnUKHSB1Q1BaHFNUzppu6JLMVn9TTPFSA \
+                            -propose-key ~/keys/my-wallet \
+                            -run-snark-worker $CODA_PK \
+                            -snark-worker-fee 1
         else
-            message = "Coda is ${status}!"
-            echo "$message"
+            message="Coda ${status}!"
+            echo "${message}"
             # Telegram notification
             # If no need to notificate just comment line bellow
-            curl -s "${tg_api}/sendMessage?chat_id=${chat_id}" \
-                --data-urlencode "text=${message}"
+            #curl -s "${tg_api}/sendMessage?chat_id=${chat_id}" \
+            #    --data-urlencode "text=${message}"
         fi
         sleep 30
-    done
+done
